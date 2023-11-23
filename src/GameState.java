ac53859b70b2596832e414a9ab0acc305b776877
Michael Speiner <speiner.m@gmail.com>
2023-11-23 12:21:45 +0100

    Skapade metod för att kunna skicka svaren i varje spelares answer array från server till klient.

--- src/GameState.java
+++ src/GameState.java
@@ -45,10 +45,10 @@ public class GameState {
     public int getWinner() {
         int player1CorrectCount = countCorrectAnswers(player1Answers);
         int player2CorrectCount = countCorrectAnswers(player2Answers);
-        if(player1CorrectCount > player2CorrectCount){
+        if (player1CorrectCount > player2CorrectCount) {
             return 1;
         }
-        if(player2CorrectCount > player1CorrectCount){
+        if (player2CorrectCount > player1CorrectCount) {
             return 2;
         }
         return 0;
@@ -65,17 +65,18 @@ public class GameState {
         return correctCount;
     }
 
-    public int getCorrectForPlayer1(){
+    public int getCorrectForPlayer1() {
         return countCorrectAnswers(player1Answers);
     }
-    public int getCorrectForPlayer2(){
+
+    public int getCorrectForPlayer2() {
         return countCorrectAnswers(player2Answers);
     }
 
     public boolean isNewRound() {
         int player1AnswerCount = countAnswers(player1Answers);
         int player2AnswerCount = countAnswers(player2Answers);
-        if(player1AnswerCount != player2AnswerCount){
+        if (player1AnswerCount != player2AnswerCount) {
             return false; // Om dom inte har lika många svar är det inte en ny runda
         }
         // Om deras svar är jämnt delbart med 2 är det ny runda
@@ -101,5 +102,55 @@ public class GameState {
             }
         }
         return true;
+    }
+
+    //Metod som konverterar arrayen svar till string array som kan skickas mellan server och klient
+    public String[] convertAnswersToStringArray(Answer[] answers) {
+        String[] stringArray = new String[answers.length];
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] != null) {
+                stringArray[i] = answers[i].toString();
+            }
+        }
+        return stringArray;
+    }
+
+    //Strings för varje separat svar
+    String answer1;
+    String answer2;
+    String answer3;
+    String answer4;
+
+    //Metod som gör om String Array till 4 separata strängar som kan skickas till klienten
+    public void sendableAnswers(String[] Answers) {
+        answer1 = Answers[0];
+        answer2 = Answers[1];
+        answer3 = Answers[2];
+        answer4 = Answers[3];
+    }
+
+    //Getters för metoder och variabler
+    public Answer[] getPlayer1Answers() {
+        return player1Answers;
+    }
+
+    public Answer[] getPlayer2Answers() {
+        return player2Answers;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
     }
 }
